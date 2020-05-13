74317e563947a76e83a387666e21ef665a410131
Guilherme Silva <guilhermebaos@gmail.com>
2020-05-13 12:48:54 +0100

    JavaScript que Atualiza os Sliders

---
+++ index.js
@@ -0,0 +1,19 @@
+// Selecionar os Sliders
+let infVelSlider = document.getElementById('inf')
+let recVelSlider = document.getElementById('rec')
+
+//Selecionar os parágrafos
+let infResp = document.getElementById('infValue')
+let recResp = document.getElementById('recValue')
+
+
+// Obter os Valores e mostrá-los
+infVelSlider.oninput = function atualizarInf() {
+    let infVel = infVelSlider.value
+    infResp.innerHTML = `${infVel}`
+}
+
+recVelSlider.oninput = function atualizarRec() {
+    let recVel = recVelSlider.value
+    recResp.innerHTML = `${recVel}`
+}
