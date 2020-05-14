da81a11da92fe023934b4e5ee472f99536ae17e0
Guilherme Silva <guilhermebaos@gmail.com>
2020-05-14 20:26:31 +0100

    Gráfico SIR Simples
    Criação do Gráfico
    Adaptação do HTML para receber o Gráfico

--- index.js
+++ index.js
@@ -10,32 +10,115 @@ let recResp = document.getElementById('recValue')
 let simplesSIR = document.getElementById('simplesSIR').getContext('2d')
 
 
+
 // Obter os Valores e mostrá-los, através de Event Listeners
 infVelSlider.oninput = function atualizarInf() {
-    let infVel = infVelSlider.value
+    let infVel = infVelSlider.value / 10
     infResp.innerHTML = `${infVel}`
     atualizarSIR()
 }
 
 recVelSlider.oninput = function atualizarRec() {
-    let recVel = recVelSlider.value
+    let recVel = recVelSlider.value / 100
     recResp.innerHTML = `${recVel}`
     atualizarSIR()
 }
 
-function atualizarSIR() {
-    let infVel = infVelSlider.value
-    let recVel = recVelSlider.value
 
-    var myChart = new Chart(simplesSIR, {
+
+// Atualizar o Gráfico
+function atualizarSIR() {
+    // Obter os arrays com as percentagens
+    let allDeltas = deltas()
+    let xTempo = allDeltas[0]
+    let dataSus = allDeltas[1]
+    let dataInf = allDeltas[2]
+    let dataRec = allDeltas[3]
+    
+    // Criar o gráfico
+    let graSimplesSIR = new Chart(simplesSIR, {
         type: 'line',
         data: {
-            labels: [1, 2, 3, 4, 5, 6, 7, 8, 9],
-            datasets: [{ 
-                data: [1, 2, 4, 3, 6, 5, 7, 8, 9],
-                label: "Africa",
-                borderColor: "#3e95cd",
-                fill: false
-              }]}
+            labels: xTempo,
+            datasets: [
+                { 
+                    data: dataSus,
+                    label: 'População Suscétivel',
+                    borderColor: 'blue',
+                    fill: false
+                },
+                {
+                    data: dataInf,
+                    label: 'População Infetada',
+                    borderColor: 'red',
+                    fill: false
+                },
+                {
+                    data: dataRec,
+                    label: 'População Removida',
+                    borderColor: 'grey',
+                    fill: true
+                }
+            ]
+        },
+        options: {
+            scales: {
+                yAxes: [
+                    {
+                        ticks: {
+                            callback: function(value, index, values) {
+                                return value + '%'
+                            }
+                        }
+                    }
+                ]
+            }
+        }
     })
 }
+
+
+// Calcular a variação de cada grupo por time-step
+function deltas() {
+    // Declarar variáveis para o cálculo da percentagem da população que está em cada grupo
+    let tempo = 0
+    let tempo_max = 200
+    let sus = 0.999
+    let inf = 1 - sus
+    let rec = 0
+
+    let xTempo = []
+    let dataSus = []
+    let dataInf = []
+    let dataRec = []
+
+    let infVel = infVelSlider.value / 10
+    let recVel = recVelSlider.value / 100
+    let resol = 0.4
+
+
+    // Calcular as derivadas por time-step
+    for(let t = tempo; t < tempo_max; t++ ) {
+
+        deltaSus = (-infVel * sus * inf) * resol
+        deltaInf = (infVel * sus * inf - recVel * inf) * resol
+        deltaRec = (recVel * inf) * resol
+
+        sus += deltaSus
+        inf += deltaInf
+        rec += deltaRec
+
+        xTempo.push(t)
+        dataSus.push(sus * 100)
+        dataInf.push(inf * 100)
+        dataRec.push(rec * 100)
+
+        if (deltaSus >= -0.00001 && deltaInf <= 0.00001 && t > 30) {
+            break
+        }
+    }
+
+    return [xTempo, dataSus, dataInf, dataRec]
+}
+
+window.onload = atualizarSIR
