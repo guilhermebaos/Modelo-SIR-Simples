62e37f67c4c52c642fd31b61f0b68cfc433bb0c3
Guilherme Silva <guilhermebaos@gmail.com>
2020-05-23 09:45:02 +0100

    Gráfico de Barras Completo

--- JS/graficoSIR.js
+++ JS/graficoSIR.js
@@ -12,15 +12,19 @@ let totInf = document.getElementById('concl-simplesSIR-totInf')
 
 // Selecionar a Div onde vão estar os gráficos
 let divSimplesSIR = document.getElementById('div-simplesSIR')
+let divBarrasSIR = document.getElementById('div-barrasSIR')
 
 
-// Obter os Valores e mostrá-los, através de Event Listeners
+// Obter os Valores e mostrá-los, através de Event Listeners, removendo os canvas antigos
 infTaxaSlider.oninput = function atualizarInf() {
     let infTaxa = infTaxaSlider.value / 20
     infResp.innerHTML = `${infTaxa.toFixed(2)}`
 
-    canvasSIR = document.getElementById('simplesSIR')
-    divSimplesSIR.removeChild(canvasSIR)
+    canvasSimplesSIR = document.getElementById('simplesSIR')
+    divSimplesSIR.removeChild(canvasSimplesSIR)
+
+    canvasBarrasSIR = document.getElementById('barrasSIR')
+    divBarrasSIR.removeChild(canvasBarrasSIR)
     atualizarSIR()
 }
 
@@ -28,8 +32,11 @@ remTaxaSlider.oninput = function atualizarRem() {
     let remTaxa = remTaxaSlider.value / 50
     remResp.innerHTML = `${remTaxa.toFixed(2)}`
 
-    canvasSIR = document.getElementById('simplesSIR')
-    divSimplesSIR.removeChild(canvasSIR)
+    canvasSimplesSIR = document.getElementById('simplesSIR')
+    divSimplesSIR.removeChild(canvasSimplesSIR)
+
+    canvasBarrasSIR = document.getElementById('barrasSIR')
+    divBarrasSIR.removeChild(canvasBarrasSIR)
     atualizarSIR()
 }
 
@@ -57,30 +64,36 @@ function maxArray(arr) {
 
 // Funções Específicas
 
-// Atualizar o Gráfico
+// Atualizar os Gráficos
 function atualizarSIR() {
-    canvasSIR = document.createElement('canvas')
+    // Criar o canvas para o gráfico de linhas
+    canvasSimplesSIR = document.createElement('canvas')
+    canvasSimplesSIR.setAttribute('id', 'simplesSIR')
+    canvasSimplesSIR.setAttribute('width', '400')
+    canvasSimplesSIR.setAttribute('height', '200')
+    divSimplesSIR.appendChild(canvasSimplesSIR)
 
-    canvasSIR.setAttribute('id', 'simplesSIR')
-    canvasSIR.setAttribute('width', '400')
-    canvasSIR.setAttribute('height', '200')
-
-    divSimplesSIR.appendChild(canvasSIR)
-
+    // Criar o canvas para o gráfico de barras
+    canvasBarrasSIR = document.createElement('canvas')
+    canvasBarrasSIR.setAttribute('id', 'barrasSIR')
+    canvasBarrasSIR.setAttribute('width', '400')
+    canvasBarrasSIR.setAttribute('height', '200')
+    divBarrasSIR.appendChild(canvasBarrasSIR)
 
     // Obter os arrays com as percentagens
     let allPoints = deltas()
     let xTempo = allPoints[0]
     let dataSus = allPoints[1]
     let dataInf = allPoints[2]
-    let dataRem = allPoints[3]
+    let dataDeltaInf = allPoints[3]
+    let dataRem = allPoints[4]
 
     // Obter os valores finais e guardá-los nas tag bold
     maxInf.innerHTML = `${maxArray(dataInf)}`
     totInf.innerHTML = `${dataRem.slice(-1)[0] + dataInf.slice(-1)[0]}`
     
-    // Criar o gráfico
-    let graSimplesSIR = new Chart(canvasSIR, {
+    // Criar o gráfico de linhas
+    let graSimplesSIR = new Chart(canvasSimplesSIR, {
         type: 'line',
         data: {
             labels: xTempo,
@@ -126,6 +139,42 @@ function atualizarSIR() {
             }
         }
     })
+
+    // Criar o gráfico de barras
+    let graBarrasSIR = new Chart(canvasBarrasSIR, {
+        type: 'bar',
+        data: {
+            labels: xTempo,
+            datasets: [
+                {
+                    data: dataDeltaInf,
+                    label: 'Novos Infetados',
+                    borderColor: 'red',
+                    backgroundColor: 'rgba(255, 0, 0, 0.7)'
+                }
+            ]
+        },
+        options: {
+            scales: {
+                xAxes: [
+                    {
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'Tempo'
+                        }
+                    }
+                ],
+                yAxes: [
+                    {
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'Número de Pessoas'
+                        }
+                    }
+                ]
+            }
+        }
+    })
 }
 
 
@@ -141,6 +190,7 @@ function deltas() {
     let xTempo = []
     let dataSus = []
     let dataInf = []
+    let dataDeltaInf = []
     let dataRem = []
 
     let infTaxa = infTaxaSlider.value / 20
@@ -168,11 +218,13 @@ function deltas() {
         // Adição aos arrays dos valores calculados
         arredRem = Number(arredondar(rem * 1000, 0))
         arredInf = Number(arredondar(inf * 1000, 0))
+        arredDeltaInf = Number(arredondar(-deltaSus * 1000, 0))
         arredSus = Number(1000 - arredInf - arredRem)
 
         xTempo.push(t)
         dataSus.push(arredSus)
         dataInf.push(arredInf)
+        dataDeltaInf.push(arredDeltaInf)
         dataRem.push(arredRem)
 
         if (deltaSus >= -0.001 && deltaRem <= 0.001 && t > 30) {
@@ -180,7 +232,7 @@ function deltas() {
         }
     }
 
-    return [xTempo, dataSus, dataInf, dataRem]
+    return [xTempo, dataSus, dataInf, dataDeltaInf, dataRem]
 }
 
 window.onload = atualizarSIR
